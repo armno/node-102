47f9f238fe619d124a5843aaa5dea16acf62bb9c
Armno <monkeyarmno@gmail.com>
2015-02-28 15:07:28 +0700

    passed chapter 11: create an http server to serve file stream

---
+++ 11-http-file-server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require('fs');
+var port = process.argv[2];
+var filePath = process.argv[3];
+
+var server = http.createServer(function(request, response) {
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+	fs.createReadStream(filePath).pipe(response);
+});
+
+server.listen(port);
